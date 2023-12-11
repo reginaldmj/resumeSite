945e4e3df1831b87bef4110e6337e4cfb996bea4
reginaldmj <reginald.jeffersonm@gmail.com>
2023-12-10 22:41:45 -0600

    admin site

--- ResumeApp/apps.py
+++ ResumeApp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ResumeappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ResumeApp'
+
+    def ready(self):
+        import ResumeApp.signals
